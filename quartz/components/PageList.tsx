d967ec7839ae7dbf3cbdedf315ed334c802a7a8b
Antoine Déchappe <dechantoine@gmail.com>
2024-12-27 15:26:41 +0100

    fix: urls redirection for folders

--- quartz/components/PageList.tsx
+++ quartz/components/PageList.tsx
@@ -1,4 +1,4 @@
-import { FullSlug, resolveRelative } from "../util/path"
+import { FullSlug, joinSegments, resolveRelative, SimpleSlug } from "../util/path"
 import { QuartzPluginData } from "../plugins/vfile"
 import { Date, getDate } from "./Date"
 import { QuartzComponent, QuartzComponentProps } from "./types"
@@ -42,6 +42,8 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
       {list.map((page) => {
         const title = page.frontmatter?.title
         const tags = page.frontmatter?.tags ?? []
+        const isFolder = tags.includes("folder")
+        const href = resolveRelative(fileData.slug!, page.slug!) + (isFolder ? "/" : "")
 
         return (
           <li class="section-li">
@@ -55,7 +57,7 @@ export const PageList: QuartzComponent = ({ cfg, fileData, allFiles, limit, sort
               </div>
               <div class="desc">
                 <h3>
-                  <a href={resolveRelative(fileData.slug!, page.slug!)} class="internal">
+                  <a href={href} class="internal">
                     {title}
                   </a>
                 </h3>
