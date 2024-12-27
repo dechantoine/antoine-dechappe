5c243cdc9dee3275c7cdc2e79f42680a24855174
Antoine Déchappe <dechantoine@gmail.com>
2024-12-27 09:01:41 +0100

    fix: paths

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://dechantoine.github.io/antoine-dechappe/",
+    baseUrl: "dechantoine.github.io/antoine-dechappe",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
