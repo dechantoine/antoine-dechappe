910bb0fefc05b0b33442a378f0e4fe310616d2f8
Antoine Déchappe <dechantoine@gmail.com>
2025-03-18 15:42:54 +0100

    feat: disable popover

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "Antoine Déchappe",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
