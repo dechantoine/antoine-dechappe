246d9b9c78e05e754da384fc40b52e28f0d85184
Antoine Déchappe <dechantoine@gmail.com>
2026-03-10 14:09:37 +0100

    ops(site): fix sitemap

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dechantoine.github.io",
+    baseUrl: "antoine-does-ai.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
